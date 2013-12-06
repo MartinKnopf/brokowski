44d8f73ac170d5306ede29c7196b4b79f8358dcf
Horsed <legendaryhorsed@gmail.com>
2013-12-06 17:14:48 +0100

    pub/sub handlers are wrapped in domain objects for hardening

--- routes/pubsub.js
+++ routes/pubsub.js
@@ -1,39 +1,62 @@
 var request
+  , domain = require('domain')
   , subscriptions = {};
 
 module.exports = function(req) {
   request = req;
 
   return {
-    publish: function(req, res) {  
-      var event = req.params['event']
-        , data = req.body;
+    publish: function(req, res) {
+      var d = domain.create();
+      runInDomain(d, req, res, function() {
+        var event = req.params['event']
+          , data = req.body;
 
-      if(subscriptions[event]) {
-        for(var i = 0, len = subscriptions[event].length; i < len; i++) {
-          var sub = subscriptions[event][i];
-          try {
-            request[sub.method.toLowerCase()]({url: sub.subscriber, json: data});
-          } catch(err) { /* ignore this subscriber and go on */ }
+        if(subscriptions[event]) {
+          for(var i = 0, len = subscriptions[event].length; i < len; i++) {
+            var sub = subscriptions[event][i];
+            try {
+              request[sub.method.toLowerCase()]({url: sub.subscriber, json: data});
+            } catch(err) { /* ignore this subscriber and go on to the next */ }
+          }
         }
-      }
 
-      res.send(200);
+        res.send(200);
+      });
     },
     subscribe: function(req, res) {
-      var event = req.params['event']
-        , sub = {subscriber: req.body.subscriber, method: req.body.method};
+      var d = domain.create();
+      runInDomain(d, req, res, function() {
+        var event = req.params['event']
+          , sub = {subscriber: req.body.subscriber, method: req.body.method};
 
-      if(sub.subscriber && sub.method && isValidHttpMethod(sub.method)) {
-        if(subscriptions[event]) subscriptions[event].push(sub);
-        else subscriptions[event] = [sub];
-        res.send(200);
-      } else {
-        res.send(405);
-      }
+        if(sub.subscriber && sub.method && isValidHttpMethod(sub.method)) {
+          if(subscriptions[event]) subscriptions[event].push(sub);
+          else subscriptions[event] = [sub];
+          res.send(200);
+        } else {
+          res.send(405);
+        }
+      });
     }    
   }
 };
+
+function runInDomain(d, req, res, f) {
+  d.on('error', function(er) {
+    console.error('error', er.stack);
+    try {
+      res.send(500);
+    } catch (er2) {
+      console.error('Error sending 500!', er2.stack);
+    }
+  });
+
+  d.add(req);
+  d.add(res);
+
+  d.run(f);
+}
 
 function isValidHttpMethod(method) {
   method = method.toLowerCase();
